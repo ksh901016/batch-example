f31278dffb2b71c39e8fc5e6f863382a57a3622e
강성현[카드결제개발] <kang.sunghyun@navercorp.com>
2021-08-20 21:48:17 +0900

    batch 예제 소스

--- src/main/java/com/example/batchexample/reactive/ReactiveExample.java
+++ src/main/java/com/example/batchexample/reactive/ReactiveExample.java
@@ -1,2 +1,37 @@
-package com.example.batchexample.reactive;public class ReactiveExample {
+package com.example.batchexample.reactive;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ReactiveExample {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(2);
+
+        Future<String> future = service.submit(() -> {
+            System.out.println("future1 : " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "complete";
+        });
+        Future<String> future2 = service.submit(() -> {
+            System.out.println("future2 : " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "complete2";
+        });
+        Thread.sleep(10000);
+        System.out.println("service : " + Thread.currentThread().getName());
+        System.out.println("result : " + future.get());
+        System.out.println("result : " + future2.get());
+
+        service.shutdown();
+    }
 }
